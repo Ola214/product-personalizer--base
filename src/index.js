d44edf10b8772f3e3f90ab9bf01cd726f88fef97
Ola214 <aleksandra.krysinska.94@gmail.com>
2025-11-10 21:27:11 +0100

    import sass

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './redux/store'
 import App from './App';
-
 import './styles/normalize.scss';
 import './styles/global.scss';
 import 'font-awesome/css/font-awesome.min.css';
@@ -9,6 +10,8 @@ import 'font-awesome/css/font-awesome.min.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
 );
